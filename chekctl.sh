7b2b295b41eebaadd06380d5f46bc91ce6b0e2ba
dranik <bohdanfil2015@gmail.com>
2022-05-11 15:10:17 +0300

    start of service

--- chekctl.sh
+++ chekctl.sh
@@ -1,8 +1,31 @@
 #!/bin/bash
-echo "Скрип для перевірки статусу програми"
-read -p "Введіть команду: " check
-if [[ `systemctl status $check | grep -o active` = "active" ]]; then
-	echo "working $check "
-else
-	echo "not working or incorrect name $check"
+statusServise()
+{
+
+read -p "Запустити цю програму? [y/n/another exit button] " select
+if [[ "$select" = "y" ]]; then
+	`systemctl start $check`
+	echo "launching $chek"
+elif [[ "$select" = "n" ]]; then
+	workCheck
+	
+else 
+	exit
 fi
+
+}
+workCheck()
+{
+read -p "Введіть команду: " check
+sS=`systemctl status $check | grep -o active`
+if [[ "$sS" = "active" ]]; then	
+	echo "$check $sS"
+else
+	echo "no work"
+	statusServise $check
+fi
+}
+
+
+workCheck
+
